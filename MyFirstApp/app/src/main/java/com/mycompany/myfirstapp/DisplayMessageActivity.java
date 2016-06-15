5c7aa0508b2bc5d680780f55a57d6ab8f060ad3a
colin <tianjiannc@gmail.com>
2016-06-15 09:22:14 +0800

    renew partial receive message function

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
@@ -1,11 +1,13 @@
 package com.mycompany.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -25,6 +27,9 @@ public class DisplayMessageActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView textView = new TextView(this);
     }
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
 
 }
