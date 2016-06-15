a2a4fbceddd5f333556c72bf15a08d9baa4e1f0c
colin <tianjiannc@gmail.com>
2016-06-15 12:37:59 +0800

    receive message and display

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -28,6 +29,10 @@ public class DisplayMessageActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
     Intent intent = getIntent();
     String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
